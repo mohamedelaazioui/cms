ac9894d3531e57aa34b581c4df1ac908ba316d30
Mohamed El Aazioui <mohamed.elaazioui@gmail.com>
2026-01-31 16:23:05 +0100

    finished stacked design

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,7 +1,18 @@
 class PublicController < ApplicationController
-    def home; end
+    def home
+      @services = Service.by_locale(I18n.locale.to_s)
+      @testimonials = Testimonial.by_locale(I18n.locale.to_s)
+    end
+    
     def about; end
-    def services; @services = Service.by_locale(I18n.locale.to_s); end
-    def testimonials; @testimonials = Testimonial.by_locale(I18n.locale.to_s); end
+    
+    def services
+      @services = Service.by_locale(I18n.locale.to_s)
+    end
+    
+    def testimonials
+      @testimonials = Testimonial.by_locale(I18n.locale.to_s)
+    end
+    
     def contact; end
 end
